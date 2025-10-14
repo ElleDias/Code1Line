f16cd8b7954d7550a5b56eef2199cde01e2105c7
Danielle Dias <danielle.c.santos9@aluno.senai.br>
2025-10-14 11:14:14 -0300

    subindo a api

--- Projeto Code1Line/obj/Debug/net8.0/Projeto Code1Line.AssemblyInfo.cs
+++ Projeto Code1Line/obj/Debug/net8.0/Projeto Code1Line.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto Code1Line")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1448917d4d0ea8107207c1d16bb11368c23039b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f116ef11192e52dc94bf32aa7fb343522f9facab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto Code1Line")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto Code1Line")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
